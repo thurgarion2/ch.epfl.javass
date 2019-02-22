005bb635aff8a6d1f678a7282ee273f31263e700
thetorf <jean-daniel.rouveyrol@epfl.ch>
2019-02-22 14:27:49 +0100

    lol

--- src/ch/epfl/javass/jass/TeamId.java
+++ src/ch/epfl/javass/jass/TeamId.java
@@ -1,5 +1,10 @@
 package ch.epfl.javass.jass;
 
 public enum TeamId {
+	TEAM_1(),
+	TEAM_2();
+	
+	public static final  List<Color> ALL = Collections.unmodifiableList(Arrays.asList(values()));
 
+	public static final int COUNT = ALL.size();
 }
