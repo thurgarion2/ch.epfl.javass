1be8474b6c436249f64483ac348fd0332661fc53
thetorf <jean-daniel.rouveyrol@epfl.ch>
2019-02-22 15:58:27 +0100

    balabala

--- src/ch/epfl/javass/jass/Score.java
+++ src/ch/epfl/javass/jass/Score.java
@@ -1,5 +1,9 @@
 package ch.epfl.javass.jass;
 
+import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
+
+import ch.epfl.javass.Preconditions;
+
 /**
  * 
  * @author Jean-Daniel Rouveyrol (301480)
@@ -9,4 +13,28 @@ package ch.epfl.javass.jass;
 //faire la copie de tout ce qui est donné dehors
 public final class Score {
 	
+	private long score;
+	
+	//à n'utiliser le constructeur que dans ofPAcked - gestion des exceptions
+	private Score(long packed) {
+		score = packed;
+	}
+	
+	public static final Score INTIAL = ofPacked(PackedScore.INITIAL);
+	
+	public static Score ofPacked(long packed) throws IllegalArgumentException{
+		Preconditions.checkArgument(PackedScore.isValid(packed));
+		return new Score(packed);
+	}
+	
+	public long packed() {
+		return score;
+	}
+	
+	public int turnTricks(TeamId t) {
+		return PackedScore.turnTricks(score, t);
+	}
+	
+	public
+
 }
