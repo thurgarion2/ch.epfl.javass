a1750b1d0dd33766c777725c50d15cea1e2b3f54
erwan serandour <erwan.serandour@epfl.ch>
2019-02-22 10:42:23 +0100

    debut commentaire.

--- src/ch/epfl/javass/Preconditions.java
+++ src/ch/epfl/javass/Preconditions.java
@@ -1,7 +1,23 @@
 package ch.epfl.javass;
 
-public class Preconditions {
-    private Preconditions() {}
+/**
+ * check preconditions and leave exception according to the result
+ * 
+ * @author erwan serandour (296100)
+ *
+ */
+public final class Preconditions {
+    private Preconditions() {
+    }
+
+    /**
+     * leave an exception according to b
+     * 
+     * @param b
+     * 
+     * @throws IllegalArgumentException
+     *             si b est vrai
+     */
 
     public static void checkArgument(boolean b) {
         if (!b) {
@@ -9,8 +25,20 @@ public class Preconditions {
         }
     }
 
+    /**
+     * check wheter the index is in the range
+     * 
+     * @param index
+     *            l'index dans l'intervalle
+     * @param size
+     *            la taille du tableau
+     * @return l'index si compatible
+     * 
+     * @throws IndexOutOfBoundsException
+     *             si l'index est hors de l'intervalle
+     */
     public static int checkIndex(int index, int size) {
-        if(index<0 || index>=size){
+        if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
         return index;
