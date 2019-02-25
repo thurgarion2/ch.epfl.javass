78b67669c647983785027f75ff110e981ff2f718
erwan serandour <erwan.serandour@epfl.ch>
2019-02-25 14:54:27 +0100

    r

--- src/ch/epfl/javass/jass/PackedScore.java
+++ src/ch/epfl/javass/jass/PackedScore.java
@@ -10,8 +10,13 @@ import ch.epfl.javass.bits.Bits64;
  *
  */
 
+/**
+ * @author esera
+ *
+ */
 public final class PackedScore {
     public static final long INITIAL=0;
+   
     
     private PackedScore() {}
     
@@ -81,11 +86,7 @@ public final class PackedScore {
        return Bits64.pack(B0To31, Integer.SIZE,B32To63, Integer.SIZE);
     }
     
-    /**
-     * @param pkScore
-     * @param t
-     * @return
-     */
+    
     private static int extractPkTeam(long pkScore, TeamId t) {
         assert isValid( pkScore);
         if(t==TeamId.TEAM_1) {
@@ -94,48 +95,103 @@ public final class PackedScore {
         return (int)Bits64.extract(pkScore,Integer.SIZE, Integer.SIZE);
     } 
     
+    /**
+     * retourne le nombre de pli gagné d'une équipe donnée
+     * 
+     * @param pkScore
+     *            le score empaqueté (valide)
+     * @param t
+     *            l'équipe
+     * @return  le nombre de pli gagné d'une équipe donnée
+     */
     public static int turnTricks(long pkScore, TeamId t) {
         assert isValid( pkScore);
         return Bits32.extract(extractPkTeam( pkScore, t), 0, 4);
     }
-    
+
+    /**
+     * retourne le nombre de de point (durant le tour) gagné d'une équipe donnée
+     * 
+     * @param pkScore
+     *            le score empaqueté (valide)
+     * @param t
+     *            l'équipe
+     * @return le nombre de de point (durant le tour) gagné d'une équipe donnée
+     */
     public static int turnPoints(long pkScore, TeamId t) {
         assert isValid( pkScore);
         return Bits32.extract(extractPkTeam( pkScore, t), 4, 9);
     }
-    
+    /**
+     * retourne le nombre de de point (durant la partie) gagné d'une équipe donnée
+     * 
+     * @param pkScore
+     *            le score empaqueté (valide)
+     * @param t
+     *            l'équipe
+     * @return le nombre de de point (durant la partie) gagné d'une équipe donnée
+     */
     public static int gamePoints(long pkScore, TeamId t) {
         assert isValid( pkScore);
         return Bits32.extract(extractPkTeam( pkScore, t), 13, 11);
     }
     
+    /**
+     * retourne le nombre de point gagné au totale durant la partie d'une équipe
+     * donnée
+     * 
+     * @param pkScore
+     *            le score empaqueté (valide)
+     * @param t
+     *            l'équipe
+     * @return le nombre de point gagné au totale durant la partie d'une équipe
+     *         donnée
+     */
     public static int totalPoints(long pkScore, TeamId t) {
         assert isValid( pkScore);
         return gamePoints( pkScore,  t)+turnPoints( pkScore,  t);
     }
     
+    /**
+     * retourne un score empqueté avec les champs mis à jour
+     * 
+     * @param pkScore
+     *            le score empaqueté de départ (valide)
+     * @param winningTeam
+     *            l'équipe gagante
+     * @param trickPoints
+     *            le nombre de points supplémentaire pour l'équipe gagnante
+     * @return un score empqueté avec les champs mis à jour en tenant compte que
+     *         k'équipe gagante à gagner trickPoints
+     */
     public static long withAdditionalTrick(long pkScore, TeamId winningTeam, int trickPoints) {
         assert isValid( pkScore);
-        assert trickPoints<257;
+        assert trickPoints<=257;
         
         int turnTricks=turnTricks( pkScore,  winningTeam)+1;
-        assert turnTricks<=9;
         int turnPoints =turnPoints( pkScore,  winningTeam)+trickPoints;
         int totalPoints =totalPoints( pkScore, winningTeam);
         
-        if(turnTricks==9) {
-            turnPoints+=100;
+        if(turnTricks==Jass.TRICKS_PER_TURN) {
+            turnPoints+=Jass.MATCH_ADDITIONAL_POINTS;
         }
          
         int winningTeamPk =oneTeamScorePk(turnTricks,  turnPoints, totalPoints);
         int losingTeamPk = extractPkTeam( pkScore, winningTeam.other());
         
         if(winningTeam==TeamId.TEAM_1) {
-            return Bits64.pack(winningTeamPk, 32,losingTeamPk , 32);
+            return Bits64.pack(winningTeamPk, Integer.SIZE,losingTeamPk , Integer.SIZE);
         }
-        return Bits64.pack(losingTeamPk, 32,winningTeamPk , 32);
+        return Bits64.pack(losingTeamPk, Integer.SIZE,winningTeamPk , Integer.SIZE);
     }
     
+    /**
+     * retourne les scores empaquetés donnés mis à jour pour le tour prochain
+     * 
+     * @param pkScore
+     *            le score empqueté
+     * @return les scores empaquetés donnés mis à jour pour le tour prochain
+     */
     public static long nextTurn(long pkScore) {
         assert isValid( pkScore);
         
@@ -148,6 +204,13 @@ public final class PackedScore {
         return "plie: "+turnTricks(pkScore, t)+" tour: "+turnPoints(pkScore,t)+" partie: "+gamePoints(pkScore,  t);
     }
     
+    /**
+     * retourne une chaine de caractère représantant le score (débuggage)
+     * 
+     * @param pkScore
+     *            le score empaqueté
+     * @return retourne une chaine de caractère représantant le score
+     */
     public static String toString(long pkScore) {
         assert isValid( pkScore);
         return teamToString( pkScore,TeamId.TEAM_1)+" / "+teamToString( pkScore,TeamId.TEAM_2);
