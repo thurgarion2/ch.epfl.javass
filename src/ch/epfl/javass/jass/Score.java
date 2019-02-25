61620930bba1b9a70bbe471a52e0897162db883f
thetorf <jean-daniel.rouveyrol@epfl.ch>
2019-02-25 08:36:13 +0100

    k

--- src/ch/epfl/javass/jass/Score.java
+++ src/ch/epfl/javass/jass/Score.java
@@ -3,6 +3,7 @@ package ch.epfl.javass.jass;
 import javax.swing.text.html.HTMLDocument.HTMLReader.PreAction;
 
 import ch.epfl.javass.Preconditions;
+import ch.epfl.javass.bits.Bits64;
 
 /**
  * 
@@ -30,11 +31,56 @@ public final class Score {
 	public long packed() {
 		return score;
 	}
-	
+	//si methode statcic --> passer attribut score en static .. que faire ??
 	public int turnTricks(TeamId t) {
 		return PackedScore.turnTricks(score, t);
 	}
 	
-	public
+	public int turnPoints(TeamId t) {
+		return PackedScore.turnPoints(score, t);
+	}
+	
+	public int gamePoints(TeamId t) {
+		return PackedScore.gamePoints(score, t);
+	}
+	
+	public int totalPoints(TeamId t) {
+		return PackedScore.totalPoints(score, t);
+	}
+	
+	
+	//le type de retour me parait chelou...
+	public Score withAdditionalTrick(TeamId winningTeam, int trickPoints) throws IllegalArgumentException{
+		if(trickPoints < 0) 
+			throw new IllegalArgumentException();
+		//esz-ce juste ?? ou qqch du type : score = .....
+		return ofPacked(PackedScore.withAdditionalTrick(score, winningTeam, trickPoints));
+	}
+	
+	//return this ??
+	public Score nextTurn() {
+		score = PackedScore.nextTurn(score);
+		return this;
+	}
+	
+	
+	public boolean equals() {
+		long s1= Bits64.extract(score, 13 ,11);
+		long s2 = Bits64.extract(score, 32+13, 11);
+		if(s1==s2)
+			return true;
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		Long s = (Long)score;
+		return s.hashCode();
+	}
+	
+	@Override
+	public String toString() {
+		return PackedScore.toString(score);
+	}
 
 }
