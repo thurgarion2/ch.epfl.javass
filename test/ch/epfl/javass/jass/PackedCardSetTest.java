cc0ec2991979d196419fbd0c52589b0acde4aa36
thetorf <jean-daniel.rouveyrol@epfl.ch>
2019-03-04 14:26:24 +0100

    de

---
+++ test/ch/epfl/javass/jass/PackedCardSetTest.java
@@ -0,0 +1,14 @@
+package ch.epfl.javass.jass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PackedCardSetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
