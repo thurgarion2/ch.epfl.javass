76e50610992483c1c8e72afa27360596f1d35459
erwan serandour <erwan.serandour@epfl.ch>
2019-03-14 13:07:49 +0100

    Player et turnstate commentée

--- src/ch/epfl/javass/jass/Player.java
+++ src/ch/epfl/javass/jass/Player.java
@@ -1,5 +1,86 @@
 package ch.epfl.javass.jass;
 
-public interface Player {
+import java.util.Map;
 
+import ch.epfl.javass.jass.Card.Color;
+
+/**
+ * défini le minimum pour se compoter comme un joueur
+ * 
+ * @author esera erwan serandour (296100)
+ *
+ */
+
+public interface Player {
+    /**
+     * retourne la carte que le joueur désire jouer selon l'état du tour et sa
+     * main
+     * 
+     * @param state
+     *            l'état courant du tour
+     * @param hand
+     *            la main du joueur
+     * @return la carte que le joueur désire jouer selon l'état du tour et sa
+     *         main
+     */
+    public abstract Card cardToPlay(TurnState state, CardSet hand);
+
+    /**
+     * (appelée seulement en début de partie) informe le joueur qu'il a
+     * l'identité ownId et que les autres joueurs sont nommés conformément au
+     * conteus de la table associative playerNames
+     * 
+     * @param ownId
+     *            l'identité du joueur
+     * @param playerNames
+     *            le nom de chaque joueur lié à leur identité
+     */
+    public default void setPlayers(PlayerId ownId,
+            Map<PlayerId, String> playerNames) {
+    }
+
+    /**
+     * informe le joueur de sa nouvelle main après un changement
+     * 
+     * @param newHand
+     *            la nouvelle main d u joueur
+     */
+    public default void updateHand(CardSet newHand) {
+    }
+
+    /**
+     * informe le joeur du changement d'atout
+     * 
+     * @param trump
+     *            le nouvel atout
+     */
+    public default void setTrump(Color trump) {
+    }
+
+    /**
+     * informe le joueur du changement du pli
+     * 
+     * @param newTrick
+     *            l'état du pli mis à jour
+     */
+    public default void updateTrick(Trick newTrick) {
+    }
+
+    /**
+     * informe le joueur du changement du score
+     * 
+     * @param score
+     *            l'état du score mis à jour
+     */
+    public default void updateScore(Score score) {
+    }
+
+    /**
+     * informe le joueur qu'une équipe à gagner (appelé qu'une seule fois)
+     * 
+     * @param winningTeam
+     *            l'équipe gagante
+     */
+    public default void setWinningTeam(TeamId winningTeam) {
+    }
 }
--- src/ch/epfl/javass/jass/TurnState.java
+++ src/ch/epfl/javass/jass/TurnState.java
@@ -2,66 +2,157 @@ package ch.epfl.javass.jass;
 
 import ch.epfl.javass.jass.Card.Color;
 
+/**
+ * l'état d'un tour de jeu
+ * 
+ * @author esera erwan serandour (296100)
+ *
+ */
+
 public final class TurnState {
-    
+
     private long score;
     private long unplayedCards;
     private int trick;
-    
-    private TurnState(long score,long unplayedCards, int trick) {
-        this.score=score;
-        this.unplayedCards=unplayedCards;
-        this.trick=trick;
+
+    private TurnState(long score, long unplayedCards, int trick) {
+        this.score = score;
+        this.unplayedCards = unplayedCards;
+        this.trick = trick;
     }
-    
-    public static TurnState initial(Color trump, Score score, PlayerId firstPlayer) {
+
+    /**
+     * retourne l'état initial correspondant à un tour de jeu dont l'atout, le
+     * score initial et le joueur initial sont ceux donnés
+     * 
+     * @param trump
+     *            l'atout initiale
+     * @param score
+     *            le score initiale
+     * @param firstPlayer
+     *            le joueur initinale
+     * @return l'état initial d'un tour de jeu
+     */
+    public static TurnState initial(Color trump, Score score,
+            PlayerId firstPlayer) {
         int pkTrick = PackedTrick.firstEmpty(trump, firstPlayer);
-        return new TurnState(score.packed(),PackedCardSet.ALL_CARDS, pkTrick);
+        return new TurnState(score.packed(), PackedCardSet.ALL_CARDS, pkTrick);
     }
-    
-    public static TurnState ofPackedComponents(long pkScore, long pkUnplayedCards, int pkTrick) {
-        if(!PackedScore.isValid(pkScore)||!PackedCardSet.isValid(pkUnplayedCards)||!PackedTrick.isValid(pkTrick)) {
+
+    /**
+     * retourne l'état dont les composantes (empaquetées) sont celles données
+     * 
+     * @param pkScore
+     *            le score courant empqueté
+     * @param pkUnplayedCards
+     *            l'ensemble des cartes non jouées
+     * @param pkTrick
+     *            le pli courant
+     * @return l'état dont les composantes (empaquetées) sont celles données
+     * @throws IllegalArgumentException
+     *             si pkScore ou pkUnplayedCards ou pkTrick ne représentent pas
+     *             score ou un set de cartes ou pli empaqeté valide
+     */
+    public static TurnState ofPackedComponents(long pkScore,
+            long pkUnplayedCards, int pkTrick) {
+        if (!PackedScore.isValid(pkScore)
+                || !PackedCardSet.isValid(pkUnplayedCards)
+                || !PackedTrick.isValid(pkTrick)) {
             throw new IllegalArgumentException();
         }
-        return new TurnState( pkScore, pkUnplayedCards,pkTrick);
+        return new TurnState(pkScore, pkUnplayedCards, pkTrick);
     }
-    
-    
+
+    /**
+     * retourne la version empaquetée du score courant
+     * 
+     * @return la version empaquetée du score courant
+     */
     public long packedScore() {
         return score;
     }
 
+    /**
+     * retourne la version empaquetée des cartes non jouées
+     * 
+     * @return la version empaquetée des cartes non jouées
+     */
     public long packedUnplayedCards() {
         return unplayedCards;
     }
 
+    /**
+     * retourne la version empaquetée du pli courant
+     * 
+     * @return la version empaquetée du pli courant
+     */
     public int packedTrick() {
         return trick;
     }
-    
+
+    /**
+     * retourne le score courant
+     * 
+     * @return le score courant
+     */
     public Score score() {
         return Score.ofPacked(score);
     }
 
+    /**
+     * retourne l'ensemble des cartes pas encore jouées
+     * 
+     * @return l'ensemble des cartes pas encore jouées
+     */
     public CardSet unplayedCards() {
         return CardSet.ofPacked(unplayedCards);
     }
 
+    /**
+     * retourne le pli courant
+     * 
+     * @return le pli courant
+     */
     public Trick trick() {
         return Trick.ofPacked(trick);
     }
-    
+
+    /**
+     * retourne vrai ssi l'état est terminal, c-à-d si le dernier pli du tour a
+     * été joué
+     * 
+     * @return vrai ssi l'état est terminal, c-à-d si le dernier pli du tour a
+     *         été joué
+     */
     public boolean isTerminal() {
-        return PackedTrick.INVALID==trick;
+        return PackedTrick.INVALID == trick;
     }
-    
+
+    /**
+     * retourne l'identité du joueur devant jouer la prochaine carte
+     * 
+     * @return l'identité du joueur devant jouer la prochaine carte
+     * @throws IllegalStateException
+     *             si le pli courant est plein
+     */
     public PlayerId nextPlayer() {
-        if(PackedTrick.isFull(trick)) {
+        if (PackedTrick.isFull(trick)) {
             throw new IllegalStateException();
         }
         return PackedTrick.player(trick, PackedTrick.size(trick));
     }
-    
+
+    /**
+     * retourne l'état correspondant après que le prochain joueur ait joué la
+     * carte donnée
+     * 
+     * @param card
+     *            la carte jouée
+     * @return l'état correspondant après que le prochain joueur ait joué la
+     *         carte donnée
+     * @throws IllegalStateException
+     *             si le pli courant est plein
+     */
     public TurnState withNewCardPlayed(Card card) {
         if (PackedTrick.isFull(trick)) {
             throw new IllegalStateException();
@@ -71,7 +162,14 @@ public final class TurnState {
         return new TurnState(score, nextUnplayedCards,
                 PackedTrick.withAddedCard(trick, card.packed()));
     }
-    
+
+    /**
+     * retourne l'état correspondant après que le pli courant ait été ramassé
+     * 
+     * @return l'état correspondant après que le pli courant ait été ramassé
+     * @throws si
+     *             le pli courant n'est pas plein
+     */
     public TurnState withTrickCollected() {
         if (!PackedTrick.isFull(trick)) {
             throw new IllegalStateException();
@@ -79,10 +177,21 @@ public final class TurnState {
         return new TurnState(score, PackedCardSet.ALL_CARDS,
                 PackedTrick.nextEmpty(trick));
     }
-    
+
+    /**
+     * retourne l'état correspondant après que le prochain joueur ait joué la
+     * carte donnée et ramassé si alors plein
+     * 
+     * @param card
+     *            la carte jouée
+     * @return l'état correspondant après que le prochain joueur ait joué la
+     *         carte donnée et ramassé si alors plein
+     * @throws IllegalStateException
+     *             si le pli courant est plein
+     */
     public TurnState withNewCardPlayedAndTrickCollected(Card card) {
-        TurnState next=withNewCardPlayed(card);
-        if(PackedTrick.isFull(next.trick)) {
+        TurnState next = withNewCardPlayed(card);
+        if (PackedTrick.isFull(next.trick)) {
             return next.withTrickCollected();
         }
         return next;
